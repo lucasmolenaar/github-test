851e73ef32960df5d6c6db3f39273400a0da9f11
lucasmolenaar <lucasmolenaar@hotmail.com>
2021-03-10 16:01:07 +0100

    tweede commit, test

--- src/nl/novi/basicprogramming/Main.java
+++ src/nl/novi/basicprogramming/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         int a = 5;
-        int b = 10;
+        int b = 11;
         int c = a + b;
         System.out.println(c);
 
