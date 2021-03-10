141b6169c3a00411a39d2ef0c23450509178f535
lucasmolenaar <lucasmolenaar@hotmail.com>
2021-03-10 16:00:17 +0100

    Eerste opzet test-document

---
+++ src/nl/novi/basicprogramming/Main.java
@@ -0,0 +1,14 @@
+package nl.novi.basicprogramming;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+        int c = a + b;
+        System.out.println(c);
+
+        Person lucas = new Person();
+        System.out.println(lucas.getName());
+    }
+}
---
+++ src/nl/novi/basicprogramming/Person.java
@@ -0,0 +1,13 @@
+package nl.novi.basicprogramming;
+
+public class Person {
+    private String name = "Lucas";
+
+    public void Person() {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
